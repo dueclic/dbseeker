1688c83fd48c4658e28c64353e5ecef4b217a30b
the404dude <alevrzz@gmail.com>
2023-12-22 15:34:57 +0100

    setup.py configured

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dbseeker',
+    version='0.4a0',
+    install_requires=['mysql-connector-python', 'tabulate'],
+    url='https://github.com/dueclic/dbseeker',
+    license='MIT',
+    author='dueclic',
+    author_email='info@dueclic.com',
+    description='A little but functional script that let you search in several databaes for an input string.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
